97249de03dad5774d32bdeb23c5ea5d0dfa9097e
Jaahanava Joshi <jaahanavajoshi@gmail.com>
2021-07-16 19:24:06 +0530

    update

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import tkinter
 import cv2
 import PIL.Image, PIL.ImageTk
+from functools import partial
+import threading
 
 
+def play(speed):
+    print(f"You clicked on play.Speed is {speed}")
 
-def play():
-    print("You clicked on play")
+def pending(decision):
+    # 1. Dispal decision pending image
+    frame = cv2.cvtColor(cv2.imread("pending.jpg"), cv2.COLOR_BGR2RGB)
+    # 2. Wait for 1 second
+    # 3. Display sponsor image
+    # 4. Wait for 1.5 seconds
+    # 5. Display out/notout image
+
+
+def out():
+    thread = threading.Thread(target=pending, args=("out",))
+    thread.daemon = 1
+    thread.start()
+    print("Player is OUT")
+
+def not_out():
+    thread = threading.Thread(target=pending, args=("not out",))
+    thread.daemon = 1
+    thread.start()
+    print("Player is NOT OUT")
+
 # Width and height of our main screen
 SET_WIDTH = 600
 SET_HEIGHT = 368
@@ -21,19 +44,19 @@ canvas.pack()
 
 
 #Buttons to control playback
-btn = tkinter.Button(window, text="<< Previous (fast)", width=50)
+btn = tkinter.Button(window, text="<< Previous (fast)", width=50, command=partial(play, -25))
 btn.pack()
-btn = tkinter.Button(window, text="<< Previous (slow)", width=50)
+btn = tkinter.Button(window, text="<< Previous (slow)", width=50, command=partial(play, -2))
 btn.pack()
-btn = tkinter.Button(window, text=" Next (slow) >>", width=50)
+btn = tkinter.Button(window, text=" Next (slow) >>", width=50, command=partial(play, 2))
 btn.pack()
-btn = tkinter.Button(window, text=" Next (fast) >>", width=50, command=play)
-btn.pack()
-
-btn = tkinter.Button(window, text=" Give Out", width=50)
+btn = tkinter.Button(window, text=" Next (fast) >>", width=50, command=partial(play, 25))
 btn.pack()
 
-btn = tkinter.Button(window, text=" Give Not Out", width=50)
+btn = tkinter.Button(window, text=" Give Out", width=50, command = out)
+btn.pack()
+
+btn = tkinter.Button(window, text=" Give Not Out", width=50, command = not_out)
 btn.pack()
 
 window.mainloop()
